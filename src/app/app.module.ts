094bca39ddd5a651fd616d3485216b3cb4e3bab3
Nathalia <nathaliacristina20@hotmail.com>
2021-01-27 22:06:20 -0300

    rename app component for rating component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
+import { RatingComponent } from './rating.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    RatingComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [RatingComponent]
 })
 export class AppModule { }
--- src/app/rating.component.ts
+++ src/app/rating.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './rating.component.html',
+  styleUrls: ['./rating.component.css']
 })
-export class AppComponent {
+export class RatingComponent {
   title = 'angular-elements-example';
 }
